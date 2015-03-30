48de3349439b17d9098838c3a0c67016a19862e4
harryzhao1 <Harryzhao@Harrys-MacBook-Pro.local>
2015-03-30 19:48:27 -0400

    yyyoooo

--- modelExactEM.py
+++ modelExactEM.py
@@ -37,61 +37,70 @@ class modelExactEM(model.model):
         
     if (self.param_pi <0).any() :
         print "param_pi <0"
+        
+    self.param_alpha[self.param_alpha < 1e-100] = 1e-100
                     
     for t in range(len(self.data)):
       # b,f
       q_bf = np.ones((self.num_class, self.num_class))
       
       for b in range(self.num_class):
-        # normB = mlab.normpdf( self.data[t], self.param_mu[b], self.param_sigma[b]) ;
-        # normB[normB < 1e-100] = 1e-100
-        # bPart = np.log( normB)
+        normB = mlab.normpdf( self.data[t], self.param_mu[b], self.param_sigma[b]) ;
+        normB[normB < 1e-100] = 1e-100
+        bPart= np.log( normB)
         
         for f in range(self.num_class):
-            toLog = self.param_alpha[f] *            mlab.normpdf( self.data[t], self.param_mu[f], self.param_sigma[f] ) +            (1 - self.param_alpha[f]) * mlab.normpdf( self.data[t], self.param_mu[b], self.param_sigma[b] );
-            # innerParts = np.ones(( len(self.data[0]), 2));
-            # normF = mlab.normpdf( self.data[t], self.param_mu[f], self.param_sigma[f] );
-            # normF[normF < 1e-100] = 1e-100
-            #
-            # innerParts[:,0] = np.log(self.param_alpha[f]) + np.log( normF );
-            # innerParts[:,1] = np.log((1 - self.param_alpha[f])) + bPart;
-            # logged = scipy.misc.logsumexp(innerParts,axis=1);
-            # assert not (toLog < 0).any()
-            toLog[toLog < 1e-100] = 1e-100
-            q_bf[b][f] = np.log(self.param_pi[b]) +  np.log(self.param_pi[f]) + np.sum(np.log(toLog)) # log domain to avoid underflow
-            # q_bf[b][f] = np.log(self.param_pi[b]) +  np.log(self.param_pi[f]) + np.sum(logged) # log domain to avoid underflow
-      
-      
-      
-      q_bf = q_bf - np.max(q_bf) # raise exponent
-      c = np.log(np.sum(np.exp(q_bf)))
-      q_bf = q_bf - c
-      q_bf = np.exp(q_bf) # back to probability domain
-      self.q_f[t] = np.sum(q_bf, axis=0)
-      self.q_b[t] = np.sum(q_bf, axis=1)
+            # toLog = self.param_alpha[f] *            mlab.normpdf( self.data[t], self.param_mu[f], self.param_sigma[f] ) +            (1 - self.param_alpha[f]) * mlab.normpdf( self.data[t], self.param_mu[b], self.param_sigma[b] );
+            innerParts = np.ones(( len(self.data[0]), 2));
+            normF = mlab.normpdf( self.data[t], self.param_mu[f], self.param_sigma[f] );
+            normF[normF < 1e-100] = 1e-100
 
-      # self.q_f[t] = np.exp(scipy.misc.logsumexp(q_bf, axis=0))
-      # self.q_b[t] = np.exp(scipy.misc.logsumexp(q_bf, axis=1))
+            innerParts[:,0] = np.log(self.param_alpha[f]) + np.log( normF );
+            innerParts[:,1] = np.log((1 - self.param_alpha[f])) + bPart;
+            logged = scipy.misc.logsumexp(innerParts,axis=1);
+            # assert not (toLog < 0).any()
+            # toLog[toLog < 1e-100] = 1e-100
+ #            q_bf[b][f] = np.log(self.param_pi[b]) +  np.log(self.param_pi[f]) + np.sum(np.log(toLog)) # log domain to avoid underflow
+            q_bf[b][f] = np.log(self.param_pi[b]) +  np.log(self.param_pi[f]) + np.sum(logged) # log domain to avoid underflow
+      
+      
+      
+      # q_bf = q_bf - np.max(q_bf) # raise exponent
+      # c = np.log(np.sum(np.exp(q_bf)))
+      # q_bf = q_bf - c
+      # q_bf = np.exp(q_bf) # back to probability domain
+      # self.q_f[t] = np.sum(q_bf, axis=0)
+      # self.q_b[t] = np.sum(q_bf, axis=1)
+
+      c2 = scipy.misc.logsumexp(q_bf);
+      q_bf = q_bf - c2; #normalized now 
+      
+      self.q_f[t] = np.exp(scipy.misc.logsumexp(q_bf, axis=0))
+      self.q_b[t] = np.exp(scipy.misc.logsumexp(q_bf, axis=1))
       
       # m
-      q_bfm = np.ones((self.num_class, self.num_class, len(self.data[0])))
-      # q_bfm0 = np.ones((self.num_class, self.num_class, len(self.data[0])))
-      # q_bfm1 = np.ones((self.num_class, self.num_class, len(self.data[0])))
+      # q_bfm = np.ones((self.num_class, self.num_class, len(self.data[0])))
+      q_bfm0 = np.ones((self.num_class, self.num_class, len(self.data[0])))
+      q_bfm1 = np.ones((self.num_class, self.num_class, len(self.data[0])))
 
       for b in range(self.num_class):
         for f in range(self.num_class):
-          q_m1 = self.param_alpha[f] * mlab.normpdf(self.data[t], self.param_mu[f], self.param_sigma[f])
-          q_m0 = (1 - self.param_alpha[f]) * mlab.normpdf(self.data[t], self.param_mu[b], self.param_sigma[b])
-          q_m1[q_m1 < 1e-100] = 1e-100
-          q_m0[q_m0 < 1e-100] = 1e-100
-          q_bfm[b,f] = q_m1 / (q_m1 + q_m0)
+            normB = mlab.normpdf( self.data[t], self.param_mu[b], self.param_sigma[b]) ;
+            normB[normB < 1e-100] = 1e-100
+            normF = mlab.normpdf( self.data[t], self.param_mu[f], self.param_sigma[f] );
+            normF[normF < 1e-100] = 1e-100
+            q_m1 = self.param_alpha[f] * normB
+            q_m0 = (1 - self.param_alpha[f]) * normF
+            q_m1[q_m1 < 1e-100] = 1e-100
+            q_m0[q_m0 < 1e-100] = 1e-100
+            # q_bfm[b,f] = q_m1 / (q_m1 + q_m0)
           # q_bfm0[b,f] = q_m0
        #    q_bfm1[b,f] = q_m1
           
           # q_m1 = np.log(self.param_alpha[f]) + np.log(mlab.normpdf(self.data[t], self.param_mu[f], self.param_sigma[f]))
  #          q_m0 = np.log((1 - self.param_alpha[f])) + np.log(mlab.normpdf(self.data[t], self.param_mu[b], self.param_sigma[b]))
- #          q_bfm0[b,f] = q_m0 / (q_m1 + q_m0);
- #          q_bfm1[b,f] = q_m1 / (q_m1 + q_m0);
+            q_bfm0[b,f] = q_m0 / (q_m1 + q_m0);
+            q_bfm1[b,f] = q_m1 / (q_m1 + q_m0);
 
       
       # q_bfm[q_bfm < 1e-6] = 1e-6
@@ -99,7 +108,7 @@ class modelExactEM(model.model):
       # q_bf3 = q_bf.reshape(self.num_class, self.num_class,1).repeat(len(self.data[0]),2);
       # self.q_tfm1[t] = np.exp(scipy.misc.logsumexp(q_bfm1 + q_bf3,axis=0))
       # self.q_tfm1[t][self.q_tfm1[t] < 1e-6] = 1e-6
-      self.q_tfm1[t] = np.sum(q_bfm * q_bf[:,:,np.newaxis],axis=0)
+      self.q_tfm1[t] = np.sum(q_bfm1 * q_bf[:,:,np.newaxis],axis=0)
       self.q_tfm1[t][self.q_tfm1[t] < 1e-100] = 1e-100
 
       #self.q_tbm1[t] = np.sum(q_bfm * q_bf[:,:,np.newaxis],axis=1)
@@ -108,7 +117,7 @@ class modelExactEM(model.model):
       #self.q_tfm0[t] = np.sum((1 -q_bfm) * q_bf[:,:,np.newaxis],axis=0)
       #self.q_tfm0[t][self.q_tfm0[t] < 1e-6] = 1e-6
       
-      self.q_tbm0[t] = np.sum((1-q_bfm) * q_bf[:,:,np.newaxis],axis=1)
+      self.q_tbm0[t] = np.sum(q_bfm0 * q_bf[:,:,np.newaxis],axis=1)
       self.q_tbm0[t][self.q_tbm0[t] < 1e-100] = 1e-100
       # self.q_tbm0[t] = np.exp(scipy.misc.logsumexp(q_bfm1 + q_bf3,axis=1))
       # self.q_tbm0[t][self.q_tbm0[t] < 1e-6] = 1e-6
@@ -124,7 +133,7 @@ class modelExactEM(model.model):
     self.param_mu = np.sum((self.q_tfm1 + self.q_tbm0) * self.data[:,np.newaxis,:], axis=0) / np.sum(self.q_tfm1 + self.q_tbm0, axis=0)
     
     # sigma
-    self.param_sigma = np.sqrt(np.sum((self.q_tfm1 +  self.q_tbm0)* pow(self.data[:,np.newaxis,:] - self.param_mu, 2), axis=0) / np.sum((self.q_tfm1 + self.q_tbm0), axis=0))
+    self.param_sigma = np.sqrt(np.sum((self.q_tfm1 + self.q_tbm0)* pow(self.data[:,np.newaxis,:] - self.param_mu, 2), axis=0) / np.sum((self.q_tfm1 + self.q_tbm0), axis=0))
     self.param_sigma[self.param_sigma < 1e-100] = 1e-100
     
     vf = np.sum(self.q_f,axis=0) / len(self.q_f)
