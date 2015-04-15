f533d9349636caa0d89079dab198507ef810854f
harryzhao1 <Harryzhao@Harrys-MacBook-Pro.local>
2015-04-15 02:13:20 -0400

    i checked more. seems like the code is fine.. cannot find any bug. I ran  a couple more times. and feels the vf and vb results are similar to the paper

--- variationalEM.py
+++ variationalEM.py
@@ -5,7 +5,7 @@ import matplotlib.cm as cm
 import matplotlib.mlab as mlab
 
 class modelVariationalEM(model.model):
-  def __init__(self,  num_class = 12, limit = None):
+  def __init__(self,  num_class = 14, limit = None):
     model.model.__init__(self,num_class, limit)
 
     # hidden variables
@@ -14,8 +14,9 @@ class modelVariationalEM(model.model):
       # 2. m representing the mask
     self.q_f = np.ones((len(self.data), self.num_class)) * 1.0 / self.num_class
     self.q_b = np.ones((len(self.data), self.num_class)) * 1.0 / self.num_class
-    self.q_tm1 = np.ones((len(self.data), len(self.data[0])))*0.5;
-    self.q_tm0 = np.ones((len(self.data), len(self.data[0])))*0.5;
+    self.q_tm1 = np.random.random((len(self.data), len(self.data[0])));
+    self.q_tm0 = 1- self.q_tm1;
+    # np.ones((len(self.data), len(self.data[0])))*0.7;
 
   def freeEnergy(self):
     return 0.0
@@ -26,9 +27,10 @@ class modelVariationalEM(model.model):
       # f
       q_ff = np.ones((self.num_class))
       for f in range(self.num_class):
+
         toLog = np.power(self.param_alpha[f] * mlab.normpdf(self.data[t],self.param_mu[f],self.param_sigma[f]) , self.q_tm1[t]) * np.power((1 - self.param_alpha[f]), self.q_tm0[t]);
-        assert not (toLog < 0).any()
-        toLog[toLog < 1e-6] = 1e-6
+        assert not (toLog < 0).any()        
+        toLog[toLog < 1e-6] = 1e-6        
         q_ff[f] = np.log(self.param_pi[f]) + np.sum(np.log(toLog)) # log domain to avoid underflow
         
       #normalize
@@ -36,7 +38,10 @@ class modelVariationalEM(model.model):
       c = np.log(np.sum(np.exp(q_ff)))
       q_ff = q_ff - c
       q_ff = np.exp(q_ff) # back to probability domain
+      q_ff[q_ff < 1e-6] = 1e-6        
+      
       self.q_f[t] = q_ff
+      
         
      
       #qm1
@@ -61,13 +66,16 @@ class modelVariationalEM(model.model):
          q_mmf0[f] = np.log(toLog) # log domain to avoid underflow
          
       for b in range(self.num_class):
-         toLog =  np.power(mlab.normpdf(self.data[t], self.param_mu[b], self.param_sigma[b]) , self.q_b[t][b] );
+         toLog =  np.power(mlab.normpdf(self.data[t], self.param_mu[b], self.param_sigma[b]) , self.q_b[t][b]);
          assert not (toLog < 0).any()
          toLog[toLog < 1e-6] = 1e-6
          q_mmb0[b] = np.log(toLog) # log domain to avoid underflow   
          
+
+               
       q_m0 = np.sum(q_mmf0, axis = 0) + np.sum(q_mmb0, axis = 0);
-     
+
+
       maxVal = np.max((np.max(q_m1), np.max(q_m0)));
       q_m1 = q_m1 - maxVal;
       q_m0 = q_m0 - maxVal;
@@ -80,10 +88,10 @@ class modelVariationalEM(model.model):
       # q_m1 = q_m1 / (q_m0 + q_m1);
       # q_m0 = np.ones((1, len(self.data[0]))) - q_m1;
      
-     
+      q_m1[q_m1 < 1e-6] = 1e-6
+      q_m0[q_m0 < 1e-6] = 1e-6
       self.q_tm1[t] =  q_m1 / (q_m0 + q_m1);
       self.q_tm0[t] =  q_m0 / (q_m0 + q_m1);     
-     
       #b
       q_bb = np.ones((self.num_class))
       
@@ -98,37 +106,15 @@ class modelVariationalEM(model.model):
       c = np.log(np.sum(np.exp(q_bb)))
       q_bb = q_bb - c
       q_bb = np.exp(q_bb) # back to probability domain
+      q_bb[q_bb < 1e-6] = 1e-6        
+      
       self.q_b[t] = q_bb
       
       
-#       # m
-#       q_bfm = np.ones((self.num_class, self.num_class, len(self.data[0])))
-#       for b in range(self.num_class):
-#         for f in range(self.num_class):
-#           q_m1 = self.param_alpha[f] * mlab.normpdf(self.data[t], self.param_mu[f], self.param_sigma[f])
-#           q_m0 = (1 - self.param_alpha[f]) * mlab.normpdf(self.data[t], self.param_mu[b], self.param_sigma[b])
-#           q_m1[q_m1 < 1e-6] = 1e-6
-#           q_m0[q_m0 < 1e-6] = 1e-6
-#           q_bfm[b,f] = q_m1 / (q_m1 + q_m0)
-#
-#       q_bfm[q_bfm < 1e-6] = 1e-6
-#
-#       self.q_tfm1[t] = np.sum(q_bfm * q_bf[:,:,np.newaxis],axis=0)
-#       self.q_tfm1[t][self.q_tfm1[t] < 1e-6] = 1e-6
-#
-# #      self.q_tbm1[t] = np.sum(q_bfm * q_bf[:,:,np.newaxis],axis=1)
-# #      self.q_tbm1[t][self.q_tbm1[t] < 1e-6] = 1e-6
-#
-# #      self.q_tfm0[t] = np.sum((1 -q_bfm) * q_bf[:,:,np.newaxis],axis=0)
-# #      self.q_tfm0[t][self.q_tfm0[t] < 1e-6] = 1e-6
-#
-#       self.q_tbm0[t] = np.sum((1 - q_bfm) * q_bf[:,:,np.newaxis],axis=1)
-#       self.q_tbm0[t][self.q_tbm0[t] < 1e-6] = 1e-6
-
   def step_M(self):
     # pi
     self.param_pi = 1.0 / (2.0 * len(self.data)) * np.sum(self.q_f + self.q_b, axis=0)
-
+        
     # alpha
     self.param_alpha = np.sum( self.q_tm1[: ,np.newaxis ,:] * self.q_f[:,:,np.newaxis], axis=0) / np.sum(self.q_f, axis = 0)[:,np.newaxis]
 
@@ -136,15 +122,17 @@ class modelVariationalEM(model.model):
     self.param_alpha[self.param_alpha > 1-1e-10] = 1-1e-10
 
     # mu
-    self.param_mu = np.sum((self.q_tm1[: ,np.newaxis ,:] * self.q_f[:,:,np.newaxis] + self.q_tm0[: ,np.newaxis ,:] * self.q_b[:,:,np.newaxis]) * self.data[:,np.newaxis,:], axis=0) / np.sum((self.q_tm1[: ,np.newaxis ,:] * self.q_f[:,:,np.newaxis] + self.q_tm0[: ,np.newaxis ,:] * self.q_b[:,:,np.newaxis]), axis=0)
+    self.param_mu =               np.sum(    (     self.q_tm1[: ,np.newaxis ,:] * self.q_f[:,:,np.newaxis] + self.q_tm0[: ,np.newaxis ,:] * self.q_b[:,:,np.newaxis]) * self.data[:,np.newaxis,:], axis=0) /                                                                            np.sum((self.q_tm1[: ,np.newaxis ,:] * self.q_f[:,:,np.newaxis] + self.q_tm0[: ,np.newaxis ,:] * self.q_b[:,:,np.newaxis]), axis=0)
 
     # sigma
-    self.param_sigma = np.sqrt(    np.sum(    (    self.q_tm1[: ,np.newaxis ,:] * self.q_f[:,:,np.newaxis] +     self.q_tm0[: ,np.newaxis ,:] * self.q_b[:,:,np.newaxis] )    *     pow(    self.data[:,np.newaxis,:] - self.param_mu, 2    ), axis=0    )     / np.sum((self.q_tm1[: ,np.newaxis ,:] * self.q_f[:,:,np.newaxis] + self.q_tm0[: ,np.newaxis ,:] * self.q_b[:,:,np.newaxis]), axis=0))
+    self.param_sigma = np.sqrt(    np.sum(    (    self.q_tm1[: ,np.newaxis ,:] * self.q_f[:,:,np.newaxis] + self.q_tm0[: ,np.newaxis ,:] * self.q_b[:,:,np.newaxis]) * pow(    self.data[:,np.newaxis,:] - self.param_mu, 2    ), axis=0    )     / np.sum((self.q_tm1[: ,np.newaxis ,:] * self.q_f[:,:,np.newaxis] + self.q_tm0[: ,np.newaxis ,:] * self.q_b[:,:,np.newaxis]), axis=0))
     self.param_sigma[self.param_sigma < 1e-6] = 1e-6
 
     vf = np.sum(self.q_f,axis=0) / len(self.q_f)
     vb = np.sum(self.q_b,axis=0) / len(self.q_b)
     
+    print vf;
+    print vb;
 if __name__ == "__main__":
   sut = modelVariationalEM(limit=None)
   sut.train()
